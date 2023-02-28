c6a199951342006adc28b0d886eb233d75073b17
Amarshankar <amarshankar.taurus@gmail.com>
2023-02-28 12:03:35 +0530

    uii

--- lib/app/modules/add_drivers/views/add_drivers_view.dart
+++ lib/app/modules/add_drivers/views/add_drivers_view.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
+import '../../../../my_theme.dart';
+import '../../../components/customized_button.dart';
+import '../../../routes/app_pages.dart';
 import '../controllers/add_drivers_controller.dart';
 
 class AddDriversView extends GetView<AddDriversController> {
@@ -9,16 +12,95 @@ class AddDriversView extends GetView<AddDriversController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('AddDriversView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'AddDriversView is working',
-          style: TextStyle(fontSize: 20),
+
+      body:Column(children: [
+        Container(
+          padding: EdgeInsets.only(left: 18, top: Get.height * .06,right: Get.width*1/5),
+          height: Get.height * .15,
+          width: double.infinity,
+          color: MyTheme.black,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.arrow_back,
+                  size: 28,
+                  color: MyTheme.white,
+                ),
+              ),
+              Text(
+                'Add Drivers',
+                style: MyTheme.regularTextStyle(
+                    color: MyTheme.white,
+                    textSize: 22,
+                    fontWeight: FontWeight.w500),
+              ),
+              SizedBox(),
+            ],
+          ),
         ),
-      ),
+        Expanded(
+          child: Container(
+            padding: EdgeInsets.only(
+                left: Get.width * .04, right: Get.width * .04, top: 24),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Wrap(
+                  runSpacing: 24,
+                  children: [
+                    Container(
+                      height: Get.height * .08,
+                      padding: EdgeInsets.only(
+                        left: Get.width * .30,
+                        right: Get.width * .30,
+                      ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.grey.shade300,
+                      ),
+                      child: Center(
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Enter Name',
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: Get.height * .08,
+                      padding: EdgeInsets.only(
+                          left: Get.width * .30, right: Get.width * .30),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.grey.shade300,
+                      ),
+                      child: Center(
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Enter Lic No',
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                CustomizedButton(
+                    colorText: MyTheme.white,
+                    cText: 'Save',
+                    colorBg: MyTheme.red,
+                    onTap: () {
+                      Get.offNamed(Routes.BUS_DASHBOARD);
+                    }),
+              ],
+            ),
+          ),
+        ),
+      ],)
     );
   }
 }
